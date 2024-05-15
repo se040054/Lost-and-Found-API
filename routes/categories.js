bdeb067edae2b6ed408a956ad75131812de8a643
se040054 <peter8822967@gmail.com>
2024-05-15 19:41:09 +0800

    typo

--- routes/categories.js
+++ routes/categories.js
@@ -5,7 +5,7 @@ const categoryController = require('../controllers/category-controller')
 const { authenticated, authenticatedAdmin } = require('../middleware/api-auth')
 
 router.get('/categories', categoryController.getCategories)
-router.get('/category/:id', categoryController.getCategory)
+router.get('/categories/:id', categoryController.getCategory)
 router.post('/categories', authenticated, authenticatedAdmin, categoryController.postCategory)
 // 管理員驗證前也需要一般登入驗證
 router.put('/categories/:id', authenticated, authenticatedAdmin, categoryController.putCategory)
